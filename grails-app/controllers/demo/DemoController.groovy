1de1d4475166737456d091f78e27912be2c5cf9a
zhangjun <jun.zhang@convertlab.com>
2022-10-09 16:05:06 +0800

    add.

--- grails-app/controllers/demo/DemoController.groovy
+++ grails-app/controllers/demo/DemoController.groovy
@@ -1,31 +1,41 @@
 package demo
 
 import com.convertlab.kafka.KafkaProducerService
-import grails.converters.JSON
+import grails.rest.RestfulController
 import org.springframework.beans.factory.annotation.Value
 
-class DemoController {
+class DemoController extends RestfulController<Demo> {
+    static responseFormats = ['json', 'xml']
+
+    DemoController(){
+        super(Demo)
+    }
 
     KafkaProducerService kafkaProducerService
 
     @Value('${kafka.demo.topic}')
     String topic
 
-    def newObject(){
-        def forTest = new Demo(
-                name: "test23",
-                enableAbtest: false
-        )
-        forTest.save()
-        render forTest as JSON
+    @Override
+    protected Demo queryForResource(Serializable id){
+        Demo.get(id)
     }
 
-    def index(String uuid){
-//        kafkaProducerService.send(topic, uuid, [demo: "demo", uuid: uuid])
-        render "5"
-    }
-
-    def redirect(){
-        forward controller: "demo", action:"index", params: [uuid:"8989"]
-    }
+//    def newObject(){
+//        def forTest = new Demo(
+//                name: "test23",
+//                enableAbtest: false
+//        )
+//        forTest.save()
+//        render forTest as JSON
+//    }
+//
+//    def index(String uuid){
+////        kafkaProducerService.send(topic, uuid, [demo: "demo", uuid: uuid])
+//        render "5"
+//    }
+//
+//    def redirect(){
+//        forward controller: "demo", action:"index", params: [uuid:"8989"]
+//    }
 }
